93e3d5e2439fbcfd233bf1a6c3f39253879813f6
ArnabChakraborty123 <chakrabortyarnab0001@gmail.com>
2024-03-18 01:20:57 +0530

    frontend fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,16 @@ const {
 module.exports = {
   darkMode: ["class"],
   content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+    "./src/**/*.{ts,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
